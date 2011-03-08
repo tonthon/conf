3bc7bdd3e5cd42d4f7b639075fcf8e595b8798a7
tonthon <tonthon21@gmail.com>
2011-03-08 23:47:32 +0100

    When it works it's better

--- install.py
+++ install.py
@@ -3,23 +3,24 @@
 Script d'installation de fichiers de configuration
 """
 from sys import argv
-from os import listdir, system
+from os import listdir, system, path
 
 HERE = path.dirname(__file__)
 
+def get_files_dir():
+    return path.join(HERE, './files/')
 def get_conf_files():
-    return [f_name for f_name in listdir(path.join(HERE, './files/'))if f_name.startswith('.')]
+    return [path.join(get_files_dir(), f_name) for f_name in listdir(get_files_dir())if f_name.startswith('.')]
 
 def copy_file(f_name, dest):
+    print("/bin/cp -f '%s' '%s' " % (f_name, dest))
     system("/bin/cp -f '%s' '%s' " % (f_name, dest))
 
 def install():
-    if len(argv) > 1:
-        destination = argv[1]
-    else:
-        destination = '~/'
+    destination = path.expanduser('~/')
 
     for f_name in get_conf_files():
-        copy_file(f_name, dest)
+        copy_file(f_name, destination)
 if __name__ == '__main__':
+    print(path.expanduser('~/'))
     install()
