4aac8c8f89b396c343f0705b73b67a6a4489d373
tonthon <tonthon21@gmail.com>
2012-07-11 14:08:10 +0200

    Adding vim files skeleton

---
+++ files/.vim/skel/tmpl.css
@@ -0,0 +1,11 @@
+:insert
+/*
+ * File Name :
+ *
+ * Copyright (C) 2012 Gaston TJEBBES g.t@majerti.fr
+ * Company : Majerti ( http://www.majerti.fr )
+ *
+ * This software is distributed under GPLV3
+ * License: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ */
